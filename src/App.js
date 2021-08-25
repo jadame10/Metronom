f161353d368645f558651ada49a1a93a4f9b6600
jadame10 <32987640+jadame10@users.noreply.github.com>
2021-08-25 18:16:14 +0200

    Metronom

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+import React from 'react';
+import { useState} from 'react';
 import './App.css';
+import click11 from './click1.wav';
 
 function App() {
+
+  const [playing, setPlaying]  = useState(false);
+  const [count, setCount] = useState(0);
+  const [bpm, setBpm] = useState(100);
+  const [beatsPerMeasure, setBeatsPerMeasure]  = useState(4);
+  const [timer, setTimer] = useState(0);
+  const click1 = new Audio(click11);
+  const [intervalId, setIntervalId] = useState(0);
+  
+const playClick = () => {
+  click1.play();
+}
+
+const handleClick = () => {
+  if (intervalId) {
+    clearInterval(intervalId);
+    setPlaying(false);
+    setIntervalId(0);
+    return;
+  }
+
+  const newIntervalId = setInterval(playClick, (60 / bpm) * 1000);
+  setIntervalId(newIntervalId);
+  setPlaying(true);
+};
+
+  const handleBpmChange = (event) =>{
+   setBpm(event.target.value);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3 className = 'm1'>Metronom</h3>
+      <div className="bpm-slider">
+          <div>{bpm} BPM</div>
+          <input type="range" min="60" max="240" value={bpm} onChange={handleBpmChange} />
+        </div>
+        <button onClick={handleClick}>{playing ? 'Stop' : 'Start'}</button>
     </div>
   );
 }
